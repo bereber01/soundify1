f716684213f564dd3e0b1730410ca6a5cb1a6d3b
bereber01 <rohankishiba44@gmail.com>
2025-05-04 10:47:30 +0300

    поправил модели песен

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -3,11 +3,11 @@ from . import views
 from .views import TrackListCreateView, TrackDetailView, UpdateSong, AddSong, index
 
 urlpatterns = [
-    path('home/', index, name='main_page'),
+    path('', index, name='main_page'),
     path('profile/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('', TrackListCreateView.as_view(), name='song-list-create'),
+    path('db/', TrackListCreateView.as_view(), name='song-list-create'),
     path('songs/&lt;int:pk&gt;/', TrackDetailView.as_view(), name='song-detail'),
     path('UpdateSong/&lt;int:pk&gt;', UpdateSong, name='UpdateSong'),
     path('AddSong', AddSong, name='AddSong'),
